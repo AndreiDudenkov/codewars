e0191d010c1105e91b3785a1e58e31c11d1c2c8c
AndreiDudenkov <andrei634d@gmail.com>
2023-03-08 11:58:20 +0300

    JS

---
+++ index13.js
@@ -0,0 +1,10 @@
+const removeChar = str => {
+    let newStr = ''
+    for(let i = 0; i < str.length; i++){
+        if(i !== 0 && i !== str.length -1) {
+            newStr += str[i]
+        }
+    }
+    console.log(newStr)
+}
+removeChar("remove")
