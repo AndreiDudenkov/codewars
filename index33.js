ffa6940804dc29282f30dc0e450072e9a0502ae7
AndreiDudenkov <andrei634d@gmail.com>
2023-03-12 19:15:27 +0300

    JS

---
+++ index33.js
@@ -0,0 +1,7 @@
+function feast(beast, dish) {
+if (beast[0] === dish[0] && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)){
+    console.log(true)
+}
+}
+
+feast("chickadee", "chocolate cake")
