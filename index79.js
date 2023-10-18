5774b3e50b5149f92b6f93f97ec6c050c840c928
AndreiDudenkov <andrei634d@gmail.com>
2023-10-19 00:07:09 +0300

    calculateYears

---
+++ index79.js
@@ -0,0 +1,13 @@
+function calculateYears(principal, interest, tax, desired) {
+
+    let years = 0;
+
+    while (principal < desired) {
+        const interestEarned = principal * interest;
+        const taxPaid = interestEarned * tax;
+        principal += interestEarned - taxPaid;
+        years++;
+    }
+
+    return years;
+}
