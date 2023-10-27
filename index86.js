529b19c11c16738b6cd38ca023503cfac1836088
AndreiDudenkov <andrei634d@gmail.com>
2023-10-28 02:37:20 +0300

    cleanString

---
+++ index86.js
@@ -0,0 +1,13 @@
+function cleanString(s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "#") {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+
+    return stack.join("");
+}
