ce679169fb7b9850cb333a444ba12241645de6ba
AndreiDudenkov <andrei634d@gmail.com>
2023-12-11 00:38:45 +0300

    isValidWalk

---
+++ index105.js
@@ -0,0 +1,11 @@
+function expandedForm(num) {
+    const digits = num.toString().split('');
+    const expandedFormArray = [];
+    for (let i = 0; i < digits.length; i++) {
+        if (digits[i] !== '0') {
+            const expandedPart = digits[i] * Math.pow(10, digits.length - 1 - i);
+            expandedFormArray.push(expandedPart);
+        }
+    }
+    return expandedFormArray.join(' + ');
+}
