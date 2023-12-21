82715eebc023d3fff3e84e588f195d4e84e85149
AndreiDudenkov <andrei634d@gmail.com>
2023-12-21 23:33:04 +0300

    humanReadable

---
+++ index110.js
@@ -0,0 +1,15 @@
+function humanReadable (seconds) {
+    if (seconds < 0 || seconds > 359999) {
+        throw new Error("Input must be a non-negative integer less than or equal to 359999");
+    }
+
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
