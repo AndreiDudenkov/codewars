4f9b014a8a5294383175bb0707f93b9e8b63f99d
AndreiDudenkov <andrei634d@gmail.com>
2024-02-06 00:33:48 +0300

    sortArray

---
+++ index114.js
@@ -0,0 +1,22 @@
+function sortArray(array) {
+    let odd = []
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2) {
+            odd.push(array[i])
+        }
+        odd.sort()
+    }
+    let a = 0
+    for (let j = 0; j < array.length; j++) {
+        if (array[j] % 2 === 0) {
+            res.push(array[j])
+        } else {
+            res.push(odd[a])
+            a++
+        }
+    }
+    return res
+}
+
+console.log(sortArray([5, 8, 6, 3, 4]))
