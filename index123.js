f3ef3a959b39893cefb478c16ef1144d03a6e6c6
AndreiDudenkov <andrei634d@gmail.com>
2024-03-04 23:57:56 +0300

    wave

---
+++ index123.js
@@ -0,0 +1,14 @@
+function wave(str){
+    const result = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') {
+            continue;  // Skip whitespace
+        }
+
+        const waveStr = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+        result.push(waveStr);
+    }
+
+    return result;
+}
